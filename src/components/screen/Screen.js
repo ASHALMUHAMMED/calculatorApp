79d50a2526e84f50764cbc581895ecf5af7ed43f
Your Name <you@example.com>
2022-09-13 22:24:58 +0530

    calculatorApp

---
+++ src/components/screen/Screen.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react'
+import { CalcContext } from '../../context/CalcContext'
+import { Textfit } from 'react-textfit';
+import '../screen/Screen.css'
+
+const Screen = () => {
+  const { calc } = useContext(CalcContext)
+
+  return (
+    <Textfit className='screen' max={70} mode="single" >{calc.num ? calc.num : calc.res}</Textfit>
+  )
+}
+
+export default Screen
